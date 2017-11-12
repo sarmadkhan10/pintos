8fdbd92773afd02d870ceb36a326bd1df26fcdc5
Hamza Anwar <hamzas.anwar@gmail.com>
2017-11-12 17:36:37 +0100

    added stubs fro project 2 in syscall.h/syscall.c and pid process identifier

--- src/userprog/syscall.c
+++ src/userprog/syscall.c
@@ -3,6 +3,7 @@
 #include <syscall-nr.h>
 #include "threads/interrupt.h"
 #include "threads/thread.h"
+#include "userprog/process.h"
 
 static void syscall_handler (struct intr_frame *);
 
@@ -11,6 +12,77 @@ syscall_init (void)
 {
   intr_register_int (0x30, 3, INTR_ON, syscall_handler, "syscall");
 }
+
+
+
+
+void syscall_halt(void){
+	//TODO: to implement
+}
+
+void syscall_exit(int status){
+	//TODO: to implement
+}
+
+
+pid_t syscall_exec(const char* cmd_line){
+	//TODO: to implement
+	return 0;
+}
+
+
+int syscall_wait(pid_t pid){
+	//TODO: to implement
+	return 0;
+}
+
+
+bool syscall_create(const char* file,unsigned initial_size){
+	//TODO: to implement
+	return false;
+}
+
+
+bool syscall_remove(const char* file){
+	//TODO: to implement
+	return false;
+}
+
+
+int syscall_open(const char* file){
+	//TODO: to implement
+	return 0;
+}
+
+
+int syscall_filesize(int fd){
+	//TODO: to implement
+	return 0;
+}
+
+
+int syscall_read(int fd,void* buffer, unsigned size){
+	//TODO: to implement
+	return 0;
+}
+
+
+void syscall_seek(int fd,unsigned position){
+	//TODO: to implement
+}
+
+
+unsigned syscall_tell(int fd){
+	//TODO: to implement
+	return 0;
+}
+
+
+void syscall_close(int fd){
+	//TODO: to implement
+}
+
+
 
 static void
 syscall_handler (struct intr_frame *f UNUSED) 
--- src/userprog/syscall.h
+++ src/userprog/syscall.h
@@ -1,6 +1,28 @@
 #ifndef USERPROG_SYSCALL_H
 #define USERPROG_SYSCALL_H
 
+#include "threads/thread.h"
+#include "userprog/process.h"
+
+//Process Identifier ID
+typedef int pid_t;
+
+
 void syscall_init (void);
+
+//user implemented methods
+void syscall_halt(void);
+void syscall_exit(int status);
+pid_t syscall_exec(const char* cmd_line);
+int syscall_wait(pid_t pid);
+bool syscall_create(const char* file,unsigned initial_size);
+bool syscall_remove(const char* file);
+int syscall_open(const char* file);
+int syscall_filesize(int fd);
+int syscall_read(int fd,void* buffer, unsigned size);
+void syscall_seek(int fd,unsigned position);
+unsigned syscall_tell(int fd);
+void syscall_close(int fd);
+
 
 #endif /* userprog/syscall.h */
