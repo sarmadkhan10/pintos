6895aba8f20fcff3af3a572c6a8497b0ed79a639
sarmadkhan10 <11beemskhan@seecs.edu.pk>
2017-11-25 14:40:26 +0100

    wait syscall complete. so is exit. minimally tested. the program exits fine

--- src/examples/echo.c
+++ src/examples/echo.c
@@ -5,11 +5,10 @@ int
 main (int argc, char **argv)
 {
   int i;
-  printf ("g");
-    printf ("re");
+
   for (i = 0; i < argc; i++)
     printf ("%s ", argv[i]);
-
+  printf ("\n");
 
   return EXIT_SUCCESS;
 }
