89a8187e52ac6bd42e52b2bb8bdf2ebda71e2b9a
Hamza Anwar <hamzas.anwar@gmail.com>
2017-12-20 03:54:01 +0100

    frame table without swapping and eviction policies.

--- src/userprog/process.c
+++ src/userprog/process.c
@@ -18,6 +18,13 @@
 #include "threads/thread.h"
 #include "threads/vaddr.h"
 #include "threads/malloc.h"
+#include "vm/frame.h"
+
+//vm frame methods
+#ifndef VM
+#define vm_frame_allocate(x) palloc_get_page(x)
+#define vm_frame_free(x) palloc_free_page(x)
+#endif
 
 static thread_func start_process NO_RETURN;
 static bool load (const char *cmdline, void (**eip) (void), void **esp);
@@ -668,14 +675,14 @@ load_segment (struct file *file, off_t ofs, uint8_t *upage, uint32_t read_bytes,
       size_t page_zero_bytes = PGSIZE - page_read_bytes;
 
       /* Get a page of memory. */
-      uint8_t *kpage = palloc_get_page (PAL_USER);
+      uint8_t *kpage = vm_frame_allocate  (PAL_USER);
       if (kpage == NULL)
   return false;
 
       /* Load this page. */
       if (file_read (file, kpage, page_read_bytes) != (int) page_read_bytes)
   {
-    palloc_free_page (kpage);
+    vm_frame_free (kpage);
     return false;
   }
       memset (kpage + page_read_bytes, 0, page_zero_bytes);
@@ -683,7 +690,7 @@ load_segment (struct file *file, off_t ofs, uint8_t *upage, uint32_t read_bytes,
       /* Add the page to the process's address space. */
       if (!install_page (upage, kpage, writable))
   {
-    palloc_free_page (kpage);
+          vm_frame_free (kpage);
     return false;
   }
 
@@ -703,7 +710,7 @@ setup_stack (void **esp, char *argv[], int argc)
   uint8_t *kpage;
   bool success = false;
 
-  kpage = palloc_get_page (PAL_USER | PAL_ZERO);
+  kpage = vm_frame_allocate (PAL_USER | PAL_ZERO);
   if (kpage != NULL)
     {
       success = install_page (((uint8_t *) PHYS_BASE) - PGSIZE, kpage, true);
